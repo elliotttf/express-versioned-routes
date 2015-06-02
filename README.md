4a713aaa688fe4cb8eee9c6e2ae34c986418251c
Elliott Foster <elliottf@codebrews.com>
2015-06-02 11:40:15 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Express Versioned Routes
+
+This middleware allows express routes to be mounted by version and provides a method
+to route between version paths and accept headers. The latter is based on the assumption
+that a version property will exist on the request object. See [express-request-version](https://www.npmjs.com/package/express-request-version)
+as a means for doing this.
+
+This middleware will mount paths from a directory that match the supported versions
+you specify. Each mounted directory's `index.js` should return either a router or valid
+middleware. For example, if the middleware is used with the base path `routes` and
+supported versions `v1`, `v2`, then the routers from `routes/v1/index.js` and
+`routes/v2/index.js` would be mounted.
+
+## Usage
+
+```javascript
+var versionRoutes = require('express-versioned-routes');
+
+app.use('/', versionRoutes(
+  path.join(__dirname, 'routes'),
+  ['v1', 'v1.1', 'v2']
+));
+```
---
+++ test/index.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var versionRoutes = require('../');
+
+module.exports = {
+  noVersionsThrowsError: function (test) {
+    test.expect(2);
+
+    test.throws(function () {
+      versionRoutes(__dirname);
+    });
+
+    test.throws(function () {
+      versionRoutes(__dirname, 'v1');
+    });
+
+    test.done();
+  },
+
+  unsupportedRouteLoaded: function (test) {
+    test.expect(1);
+
+    test.throws(function () {
+      versionRoutes(path.join(__dirname, 'routes'), ['v2']);
+    });
+
+    test.done();
+  },
+
+  mountVersionedRoute: function (test) {
+    test.expect(4);
+
+    var wares = versionRoutes(path.join(__dirname, 'routes'), ['v1']);
+
+    test.equal(wares.length, 2, 'Subrouter not mounted correctly.');
+
+    wares[1]({}, {}, function () {
+      test.ok(true, 'Unversioned request passes to next.');
+      wares[1]({ version: 'v2' }, {}, function () {
+        test.ok(true, 'Mismatched version passes to next.');
+        wares[1]({ version: 'v1' }, {}, function () {
+          test.ok(true, 'Versioned route handled.');
+          test.done();
+        });
+      });
+    });
+  }
+};
+
