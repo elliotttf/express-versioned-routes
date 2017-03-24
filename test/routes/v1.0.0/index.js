16147d25ccb370b06b5597b56f819ce1b412f935
Elliott Foster <elliottf@codebrews.com>
2017-03-24 11:04:45 -0500

    fix(lint): Remove multi-assignments to satisfy new lint rules

--- test/routes/v1.0.0/index.js
+++ test/routes/v1.0.0/index.js
@@ -2,7 +2,8 @@
 
 const express = require('express');
 
-const router = module.exports = express.Router();
+const router = express.Router();
+module.exports = router;
 
 router.get('/users', (req, res) => res.send({ version: req.version }));
 
